c8283dd4598436e7738a77b02d8dff361b90dc08
stephanielam0506 <stephanielamtw@gmail.com>
2024-02-09 17:09:12 -0800

    swicthed repository.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+$('.button').click(function() {
+  $('.button').css({
+    'background-color': 'red'
+  })
+});
+
+// {/* <script type="text/javascript">
+//     document.getElementById("myButton").onclick = function () {
+//         location.href = "www.yoursite.com";
+//     };
+// </script> */}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
